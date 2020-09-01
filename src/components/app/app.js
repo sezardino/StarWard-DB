f5f20a6acc4227a1c884c61420a6632b38aa1a16
Edward <e.arekhva@gmail.com>
2020-09-01 13:14:21 +0200

    error boundary in use

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,22 +1,47 @@
 import React, {Component} from 'react';
+import ErrorBoundary from '../error-boundary';
 
 import Header from '../header';
 import RandomPlanet from '../random-planet';
 import PersonPage from '../person-page';
-// import ItemList from '../item-list';
-// import PersonDetails from '../person-details';
+import ItemList from '../item-list';
+import ItemDetails from '../item-details';
 
 import './app.css';
+import DataBase from '../../api';
 
 export default class App extends Component {
+	DataBase = new DataBase();
+
 	render() {
 		return (
-			<div>
+			<ErrorBoundary>
 				<Header />
 				<RandomPlanet />
-
 				<PersonPage />
-			</div>
+
+				{/* <div className="row mb2">
+					<div className="col-md-6">
+						<ItemList
+							getData={this.DataBase.getAllStarships}
+							onListItemClick={this.listItemHandler}
+							renderItem={({name, model}) => `${name} | model: ${model}`}
+						/>
+					</div>
+					{/* <div className="col-md-6"><PersonDetails dataId={selectedItem} /></div> */}
+				{/* </div> */}
+
+				{/* <div className="row mb2">
+					<div className="col-md-6">
+						<ItemList
+							getData={this.DataBase.getAllPlanets}
+							onListItemClick={this.listItemHandler}
+							renderItem={({name, diameter}) => `${name} | diameter: ${diameter}`}
+						/>
+					</div>
+					{/* <div className="col-md-6"><PersonDetails dataId={selectedItem} /></div> */}
+				{/* </div>  */}
+			</ErrorBoundary>
 		);
 	}
 }
