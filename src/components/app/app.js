7db6776fa5f8b4cb76b0de820f4ea06eeae9bf49
Edward <e.arekhva@gmail.com>
2020-09-04 12:08:10 +0200

    add swich and redirect when page not found

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import ErrorBoundary from '../error-boundary';
 import {PersonPage, PlanetPage, StarshipPage, SecretPage, LoginPage} from '../pages/';
 import {RandomPlanet, StarshipDetails} from '../app-components';
@@ -21,24 +21,27 @@ export default class App extends Component {
 					<Header />
 					<RandomPlanet />
 
-					<Route path="/" exact render={() => <h1>Welcome on Star Wars Data Base</h1>} />
-					<Route path="/people/:id?" exact component={PersonPage} />
-					<Route path="/planets" exact component={PlanetPage} />
-					<Route path="/starships" exact component={StarshipPage} />
-					<Route
-						path="/starships/:id"
-						render={({
-							match: {
-								params: {id},
-							},
-						}) => <StarshipDetails dataId={id} />}
-					/>
-					<Route
-						path="/login"
-						exact
-						render={() => <LoginPage isLoggedIn={isLoggedIn} onLogin={this.logInHandler} />}
-					/>
-					<Route path="/secret" exact render={() => <SecretPage isLoggedIn={isLoggedIn} />} />
+					<Switch>
+						<Route path="/" exact render={() => <h1>Welcome on Star Wars Data Base</h1>} />
+						<Route path="/people/:id?" exact component={PersonPage} />
+						<Route path="/planets" exact component={PlanetPage} />
+						<Route path="/starships" exact component={StarshipPage} />
+						<Route
+							path="/starships/:id"
+							render={({
+								match: {
+									params: {id},
+								},
+							}) => <StarshipDetails dataId={id} />}
+						/>
+						<Route
+							path="/login"
+							exact
+							render={() => <LoginPage isLoggedIn={isLoggedIn} onLogin={this.logInHandler} />}
+						/>
+						<Route path="/secret" exact render={() => <SecretPage isLoggedIn={isLoggedIn} />} />
+						<Route render={() => <h2>Page not found</h2>} />
+					</Switch>
 				</Router>
 			</ErrorBoundary>
 		);
