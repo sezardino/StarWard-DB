9a3695fa2cb9856d817cfa40d7e9b1e2cfe4be59
Edward <e.arekhva@gmail.com>
2020-09-05 21:52:00 +0200

    Revert "Revert "Updates""
    This reverts commit 4b53d367a0f2e42d189814a4e7e90e752311f493.

---
+++ precache-manifest.4720bed4349708542f89945f66537118.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb33cace0588f7309faf8288e2aa1450",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7887a065aa8112145e9e",
+    "url": "/static/css/main.b4bc6c62.chunk.css"
+  },
+  {
+    "revision": "79f510ec899ea559be54",
+    "url": "/static/js/2.5e797c05.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.5e797c05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7887a065aa8112145e9e",
+    "url": "/static/js/main.46abad7a.chunk.js"
+  },
+  {
+    "revision": "a084c359ae51ab406491",
+    "url": "/static/js/runtime-main.a7c7f116.js"
+  }
+]);
