0e8fd94d52b1cc4dd01641a5c515975da99168ed
Edward <e.arekhva@gmail.com>
2020-09-05 21:37:42 +0200

    Updates

---
+++ precache-manifest.63c9ed720d69fac866ee8be35b7a539b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27fd582c84de124007fcafae59f7e709",
+    "url": "/starward-db/index.html"
+  },
+  {
+    "revision": "7887a065aa8112145e9e",
+    "url": "/starward-db/static/css/main.b4bc6c62.chunk.css"
+  },
+  {
+    "revision": "79f510ec899ea559be54",
+    "url": "/starward-db/static/js/2.5e797c05.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/starward-db/static/js/2.5e797c05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7887a065aa8112145e9e",
+    "url": "/starward-db/static/js/main.46abad7a.chunk.js"
+  },
+  {
+    "revision": "325abbd06dc342c9b7a3",
+    "url": "/starward-db/static/js/runtime-main.f9241c63.js"
+  }
+]);
